7ceb0ef5a418da84ea57c98f752578fb692fcfb0
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-07 21:23:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-     res.send(createTemplate(articleOne));
+     res.send(createTemplate(article-one));
 });
 app.get('/article-two', function (req, res) {
     res.send("article two requested and will be served");
