88c56a353503cce765fa1dc99627f5b45489559e
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-07 21:19:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,62 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content = {
+    title: 'ARTICLE ONE | TEJASWINI SRIDHAR',
+    date: 'august 7th 2017',
+    content:  `<p>
+        These lines are the from the book "THE PRIDE AND PREJUDICE".
+        </p>
+        <p>
+        I cannot fix on the hour, or the spot, or the look or the words, which laid the foundation. It is too long ago. I was in the middle before I knew that I had begun.
 
+      </p>
+      <p>
+        I declare after all there is no enjoyment like reading! How much sooner one tires of any thing than of a book! -- When I have a house of my own, I shall be miserable if I have not an excellent library.
+        -Jane Austen, Pride and Prejudice
+      </p>`
+};
+function createTemplate (data) {
+    var tile=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate=
+`<html>
+  <head>
+    <title>
+      ${title}
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link href="/ui/style.css" rel="stylesheet" />
+     
+      </head>
+  <body>
+      <div class="container">
+    <div>
+      <a  href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+    </h3>
+    <div>
+    ${date}
+    </div>
+    <div>
+      ${content}
+    </div>
+    </div>
+  </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
     res.send("article two requested and will be served");
