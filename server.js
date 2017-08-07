0a1e041da3532519ac4157d5b5437316ac678711
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-07 19:56:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
-    res.send("article one requested and will be served");
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'
 });
 app.get('/article-two', function (req, res) {
     res.send("article two requested and will be served");
