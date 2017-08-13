293865bd77d3752a6d2a78aaf670dac53e8da258
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-13 12:48:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 //move the image
-var img = document.getElementbyId('madi');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
