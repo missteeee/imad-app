dc3992ecc0db1004f35be87504745e107508cf5c
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-13 15:01:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ if(request.status===200) {
         //make a request to the server and send the name
         
         // capture a list of names and render it as a list
-        var names=['name1','name2','name3','name4'];
+        var names=['PS-Sridhar','Veena','Yashaswini'];
         var list='';
         for(var i=0;i<names.length;i++){
             list += '<li>' + names[i] + '<li>'
