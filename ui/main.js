1abc9fbf34fe786ea2913dda390ca0c890b7fb78
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-13 12:30:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main text');
 element.innerHTML='new value';
-var img=document.getElementbyId('ruhi');
+var img=document.getElementbyId('madi');
 img.onclick = function() {
-  img.style.marginLeft = '100px';  
+  img.style.marginLeft = '100px'; 
+  var interval=setInterval(moveRight,100); 
+    
 };
