0c47eca21d6081bc6d64d64ade9ae7aa05ceae78
missteeee <30799452+missteeee@users.noreply.github.com>
2017-08-13 14:32:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,19 @@ if(request.status===200) {
     request.open('GET', 'http://tejaswinisridhar25.imad.hasura-app.io/counter',true);
     request.send(null);
     };
+    //submit name
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    var submit=document.getElementById('submit_btn');
+    submit.onclick=function(){
+        //make a request to the server and send the name
+        
+        // capture a list of names and render it as a list
+        var names=['name1','name2','name3','name4'];
+        var list='';
+        for(var i=0;i<names.length;i++){
+            list += '<li>' + names[i] + '<li>'
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML=list;
+    };
